648f66cec76f9d06925dd4eb3b13db80911f755f
Stonesitter <maalthaler@gmail.com>
2025-10-15 12:32:46 +0200

    snapshot: working tree before removing hint button

--- src/components/BrandLogoNav.js
+++ src/components/BrandLogoNav.js
@@ -1,44 +1,28 @@
 import React, { useContext } from "react";
 import { SITE_NAME, SITE_VERSION, ThemeContext } from "../config/config";
-import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
 
+console.log("BrandLogoNav: loaded new version");
+
 const useStyles = makeStyles({
-    "siteNavLink": {
+    siteNavLink: {
         textAlign: 'left',
-        paddingTop: "3px",
-        "&:hover": {
-            cursor: "pointer"
-        }
+        paddingTop: "6px",
+        cursor: "default",
+        userSelect: "none",
     }
-})
+});
 
-function BrandLogoNav({ isPrivileged = false, noLink = false }) {
-    const context = useContext(ThemeContext)
-    const history = useHistory()
+function BrandLogoNav() {
+    const context = useContext(ThemeContext);
+    const classes = useStyles();
+    const brandString = `${SITE_NAME} (v${SITE_VERSION})`;
 
-    const classes = useStyles()
-    const brandString = `${SITE_NAME} (v${SITE_VERSION})`
-
-    const navigateLink = (evt) => {
-        if (evt.type === "click" || evt.key === "Enter") {
-            history.push("/")
-        }
-    }
-
-    return <>
-        {/* specified to not link or was launched from lms as student*/}
-        {noLink || (context.jwt.length !== 0 && !isPrivileged)
-            ? <div style={{ textAlign: 'left', paddingTop: 6 }}>
-                {brandString}
-            </div>
-            :
-            <div role={"link"} tabIndex={0} onClick={navigateLink} onKeyDown={navigateLink}
-                 className={classes.siteNavLink}>
-                {brandString}
-            </div>
-        }
-    </>
+    return (
+        <div className={classes.siteNavLink}>
+            {brandString}
+        </div>
+    );
 }
 
-export default BrandLogoNav
+export default BrandLogoNav;
